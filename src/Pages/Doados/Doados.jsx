99f55d13517d1532709a7d1e7ad6cd40c8446596
Mayk Baldon <mayk_mbrs@hotmail.com>
2024-12-04 19:23:52 -0300

    entrega
    responsivo pronto.

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -12,6 +12,12 @@ export default function Doados(){
                     <p>Susanne Andrade</p>
                     <p>Ficção</p>
                 </article>
+                <article>
+                    <img src={livro} alt="imagem do livro o protagonista" />
+                    <h3>O Protagonista</h3>
+                    <p>Susanne Andrade</p>
+                    <p>Ficção</p>
+                </article>
             </section>
         </section>
     )
