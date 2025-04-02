1c5b033837a216ce9783ac3b1b15a2f2d6d1698c
Mayk Baldon <mayk_mbrs@hotmail.com>
2025-04-02 19:36:03 -0300

    modificações no quero doar

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,7 +1,25 @@
 import S from './doados.module.scss'
-import livro from '../../assets/book.png'
+import livro from '../../assets/livro.png'
+import axios from 'axios'
+import {useState,useEffect} from 'react'
+
+
 
 export default function Doados(){
+
+    const [livros, setLivros] = useState([])
+
+    const getLivros = async () => {
+        const response = await axios.get("Link da sua API")
+        setLivros(response.data)
+    }
+
+    useEffect(()=>{
+        getLivros()
+    },[])
+
+
+
     return(
         <section className={S.boxDoados}>
             <h2>Livros Doados</h2>
@@ -12,13 +30,18 @@ export default function Doados(){
                     <p>Susanne Andrade</p>
                     <p>Ficção</p>
                 </article>
-                <article>
-                    <img src={livro} alt="imagem do livro o protagonista" />
-                    <h3>O Protagonista</h3>
-                    <p>Susanne Andrade</p>
-                    <p>Ficção</p>
-                </article>
+                {livros.map((item)=>(
+                    <article key={item.id}>
+                        <img src={item.imagem_url} alt="" />
+                        <h3>{item.titulo}</h3>
+                        <p>{item.categoria}</p>
+                        <p>{item.autor}</p>
+                    </article>
+
+                ))}
+
+
             </section>
         </section>
-    )
+    )
 }
